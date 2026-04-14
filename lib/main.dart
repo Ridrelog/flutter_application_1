c17e8e88b41e0f810bcfbd73db04299ba8195ada
IfZuni <farrizgm@gmail.com>
2026-04-14 13:16:17 +0700

    a

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ class WelcomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
